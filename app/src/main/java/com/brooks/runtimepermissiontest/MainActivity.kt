0f8fe8d4b44a888731cd5c05b89688db38761fbc
brooks <myt2000@126.com>
2022-11-04 16:30:08 +0800

    增加makeCall button按钮，添加ACTION_CALL权限

--- app/src/main/java/com/brooks/runtimepermissiontest/MainActivity.kt
+++ app/src/main/java/com/brooks/runtimepermissiontest/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.brooks.runtimepermissiontest
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        makeCall.setOnClickListener {
+            try {
+                val intent = Intent(Intent.ACTION_CALL)
+                intent.data = Uri.parse("tel:10086")
+                startActivity(intent)
+            } catch (e: SecurityException){
+                e.printStackTrace()
+            }
+        }
     }
 }
