d4437c4ac7960a5a1a1f361509509000a335e6ee
brooks <myt2000@126.com>
2022-11-07 11:07:09 +0800

    直接拨打电话，申请拨打电话的权限

--- app/src/main/java/com/brooks/runtimepermissiontest/MainActivity.kt
+++ app/src/main/java/com/brooks/runtimepermissiontest/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.brooks.runtimepermissiontest
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +16,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         makeCall.setOnClickListener {
-            try {
-                val intent = Intent(Intent.ACTION_CALL)
-                intent.data = Uri.parse("tel:10086")
-                startActivity(intent)
-            } catch (e: SecurityException){
-                e.printStackTrace()
+            // 权限检查
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                // 权限请求
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1)
+            } else {
+                call()
             }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
         }
     }
 }
